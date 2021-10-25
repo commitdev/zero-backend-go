6cc87c3ca6b5596010ed3fd07ae17e75a0f2c33c
Deven96 <diretnandomnan@gmail.com>
2021-10-25 20:29:12 +0100

    added cachestore.TestConnection to fix #84 and #85 (#88)

---
+++ templates/internal/cachestore/cachestore.go
@@ -0,0 +1,42 @@
+package cachestore
+
+import (
+	"os"
+	"fmt"
+<%if eq (index .Params `cachestore`) "redis" %>	
+	"context"
+	"github.com/go-redis/redis/v8"
+<% end %>
+<%if eq (index .Params `cachestore`) "memcached" %>	
+	"github.com/bradfitz/gomemcache/memcache"
+<% end %>
+)
+
+var cacheEndpoint = os.Getenv("CACHE_ENDPOINT")
+var cachePort = os.Getenv("CACHE_PORT")
+
+<%if eq (index .Params `cachestore`) "redis" %>	
+func TestConnection() {
+	context := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", cacheEndpoint, cachePort),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "cache-test", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+<% end %>
+
+<%if eq (index .Params `cachestore`) "memcached" %>	
+func TestConnection() {
+	mc := memcache.New(fmt.Sprintf("%s:%s", cacheEndpoint, cachePort))
+	err := mc.Set(&memcache.Item{Key: "cache-test", Value: []byte("value")})
+	if err != nil {
+		panic(err)
+	}
+}
+<% end %>
